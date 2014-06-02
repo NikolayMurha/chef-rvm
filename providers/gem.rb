5594915e6a9b773431be77a18e59e60568fbfe71
Nikolay Murga <work@murga.kiev.ua>
2014-06-02 22:24:08 +0300

    Fix detect version

--- providers/gem.rb
+++ providers/gem.rb
@@ -2,7 +2,7 @@ include Chef::Cookbook::RVM::EnvironmentFactory
 [:install, :update, :uninstall].each do |action_name|
   action action_name do
     Chef::Log.debug("#{action_name.to_s.capitalize} #{new_resource.gem} in #{new_resource.ruby_string} from user #{new_resource.user}")
-    raise "Can't install gem #{new_resource.gem} because ruby #{new_resource.ruby_string} not installed!" unless env.use(new_resource._version)
+    raise "Can't install gem #{new_resource.gem} because ruby #{new_resource._version} not installed!" unless env.use(new_resource._version)
     if new_resource._gemset
       unless env.gemset_use(new_resource._gemset)
         Chef::Log.info("Create gemset for #{new_resource.ruby_string}!")
