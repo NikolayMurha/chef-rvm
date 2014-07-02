f350cb05e46c08006179fe079648b3c60793f0ba
Nikolay Murga <work@murga.kiev.ua>
2014-07-02 23:13:59 +0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'cookbooks@randrmusic.com'
 license          'All rights reserved'
 description      'Installs/Configures rvm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends          'sudo'
