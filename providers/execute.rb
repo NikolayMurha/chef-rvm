c8547ca2147417558f61e9b8dedec10466f34662
Nikolay Murga <nikolay.m@murga.kiev.ua>
2014-11-03 23:02:31 +0200

    Fix execute resource

--- providers/execute.rb
+++ providers/execute.rb
@@ -9,16 +9,16 @@ def whyrun_supported?
 end
 
 action :do do
-  converge_by("Execute: rvm #{new_resource.environment} do #{new_resource.command}") do
-    env.rvm(new_resource.environment, :do, new_resource.command)
+  converge_by("Execute: rvm #{new_resource.ruby_string} do #{new_resource.command}") do
+    env.rvm(Array(new_resource.ruby_string).join(','), :do, new_resource.command)
     new_resource.updated_by_last_action(true)
     Chef::Log.info("#{@new_resource} ran successfully")
   end
 end
 
 action :in do
-  converge_by("Execute: rvm do #{new_resource.environment} in #{new_resource.command}") do
-    env.rvm(:in, new_resource.environment, :do, new_resource.command)
+  converge_by("Execute: rvm in #{new_resource.path} do #{new_resource.command}") do
+    env.rvm(:in, new_resource.path, :do, new_resource.command)
     new_resource.updated_by_last_action(true)
   end
 end
--- resources/execute.rb
+++ resources/execute.rb
@@ -3,5 +3,5 @@ actions :create, :delete, :update, :pristine, :prune
 default_action :create
 attribute :command, kind_of: [String], name_attribute: true, :required => true
 attribute :user, kind_of: [String], default: 'root'
-attribute :environment, kind_of: [Array], :required => true
-attribute :ruby_string, kind_of: [String], :required => true
+attribute :path, kind_of: [String], :required => false
+attribute :ruby_string, kind_of: [String,Array], :required => false
