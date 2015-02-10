7b5532855ce5cdd578e762f9c06afc238edb43dd
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-02-10 16:13:39 +0200

    Fix rvm LWRP name

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,7 +1,7 @@
 include_recipe 'ruby_rvm::packages'
 
 node['rvm']['users'].each do |name, options|
-  rvm name do
+  ruby_rvm name do
     action options['action'] if options
   end
 end if node['rvm']
